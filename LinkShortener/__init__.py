9aa0929e870c77068cdd6ef5c78a4b512458d269
campenr <richard@campen.co>
2017-07-01 15:57:47 -0400

    Split the api into it's own file.

--- LinkShortener/__init__.py
+++ LinkShortener/__init__.py
@@ -35,6 +35,4 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
-from LinkShortener import views, models, forms
-
-app.jinja_env.filters['date_filter'] = models.Link.format_date
+from LinkShortener import views, models, forms, api
