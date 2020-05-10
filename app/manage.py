38053445c1522f49c92477712a9baa7c8af2ef30
Richard Campen <richard@campen.co>
2020-05-10 14:03:02 +1200

    create initial django app project structure and move old flask app to app_old

--- app/manage.py
+++ app/manage.py
@@ -1,22 +1,21 @@
-import sys
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
+import sys
 
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
-from app import flask_app, db
-
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-app = flask_app
-
-MIGRATION_DIR = os.path.join(PROJECT_DIR, 'app', 'migrations')
-
-migrate = Migrate(app, db, directory=MIGRATION_DIR)
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    manager.run()
+    main()
