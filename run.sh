5e3ecd0438808197587e2915a20e49c876998b54
Richard Campen <richard@campen.co>
2020-08-02 20:57:06 +1200

    feat: update ansible modules to bring up to date with latest state.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 args="$@"
-vagrant ssh -c "tail -f -n0 ./logs/uwsgi/link-shortener.log & sudo uwsgi --ini ./config/uwsgi/link-shortener.ini $args"
+vagrant ssh -c "grc tail -f -n0 ./logs/uwsgi/link-shortener.log & sudo uwsgi --ini ./config/uwsgi/link-shortener.ini $args"
